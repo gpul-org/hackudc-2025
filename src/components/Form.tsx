901eefc82f97123bc4cd2a5479a8fcf6c12568a8
Delthia <git@delthia.com>
2024-11-11 20:22:49 +0100

    Formulario de registro

--- src/components/Form.tsx
+++ src/components/Form.tsx
@@ -4,7 +4,16 @@ import {
   faEnvelope,
   faUser,
 } from '@fortawesome/free-regular-svg-icons'
-import { faArrowRight } from '@fortawesome/free-solid-svg-icons'
+import {
+  faArrowRight,
+  faPhone,
+  faCalendar,
+  faUserGraduate,
+  faLocationPin,
+  faSchool,
+  faWheatAwnCircleExclamation,
+  faShirt
+} from '@fortawesome/free-solid-svg-icons'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { navigate } from 'astro:transitions/client'
 import { useState } from 'react'
@@ -59,36 +68,224 @@ export default function Form() {
     return (
       <div id="waitlist-form">
         <form className="space-y-5" onSubmit={handleSubmit}>
+          <div class="flex flex-row gap-4">
+            <div className="relative w-full">
+              <span className="absolute inset-y-0 left-0 flex items-center pl-3">
+                <FontAwesomeIcon
+                  icon={faUser}
+                  className="h-5 w-5 text-white/50"
+                />
+              </span>
+              <input
+                id="nameInput"
+                type="text"
+                required
+                className="block w-full rounded-lg border border-white/20 bg-white/5 p-2.5 pl-10 font-light text-white placeholder-white/50 focus:border-transparent focus:outline-none focus:ring-[1px] focus:ring-green-500 focus:ring-opacity-60"
+                placeholder="Introduce tu nombre"
+              />
+            </div>
+            <div className="relative w-full">
+              <span className="absolute inset-y-0 left-0 flex items-center pl-3">
+                <FontAwesomeIcon
+                  icon={faEnvelope}
+                  className="h-5 w-5 text-white/50"
+                />
+              </span>
+              <input
+                id="emailInput"
+                type="email"
+                required
+                className="block w-full rounded-lg border border-white/20 bg-white/5 p-2.5 pl-10 font-light text-gray-300 placeholder-white/50 focus:border-transparent focus:outline-none focus:ring-[1px] focus:ring-green-500 focus:ring-opacity-60"
+                placeholder="Un correo electrónico"
+              />
+            </div>
+          </div>
+          <div class="flex flex-row gap-4">
+            <div className="relative w-full">
+              <span className="absolute inset-y-0 left-0 flex items-center pl-3">
+                <FontAwesomeIcon
+                  icon={faPhone}
+                  className="h-5 w-5 text-white/50"
+                />
+              </span>
+              <input
+                id="phoneInput"
+                type="text"
+                required
+                className="block w-full rounded-lg border border-white/20 bg-white/5 p-2.5 pl-10 font-light text-gray-300 placeholder-white/50 focus:border-transparent focus:outline-none focus:ring-[1px] focus:ring-green-500 focus:ring-opacity-60"
+                placeholder="Un número de teléfono"
+              />
+            </div>
+            <div className="relative w-full">
+              <span className="absolute inset-y-0 left-0 flex items-center pl-3">
+                <FontAwesomeIcon
+                  icon={faCalendar}
+                  className="h-5 w-5 text-white/50"
+                />
+              </span>
+              <input
+                id="calendarInput"
+                type="text"
+                required
+                className="block w-full rounded-lg border border-white/20 bg-white/5 p-2.5 pl-10 font-light text-gray-300 placeholder-white/50 focus:border-transparent focus:outline-none focus:ring-[1px] focus:ring-green-500 focus:ring-opacity-60"
+                placeholder="Un número de teléfono"
+              />
+            </div>
+          </div>
           <div className="relative">
             <span className="absolute inset-y-0 left-0 flex items-center pl-3">
               <FontAwesomeIcon
-                icon={faUser}
+                icon={faSchool}
                 className="h-5 w-5 text-white/50"
               />
             </span>
             <input
-              id="nameInput"
+              id="studyLocationInput"
               type="text"
               required
-              className="block w-full rounded-lg border border-white/20 bg-white/5 p-2.5 pl-10 font-light text-white placeholder-white/50 focus:border-transparent focus:outline-none focus:ring-[1px] focus:ring-green-500 focus:ring-opacity-60"
-              placeholder="Introduce tu nombre"
+              className="block w-full rounded-lg border border-white/20 bg-white/5 p-2.5 pl-10 font-light text-gray-300 placeholder-white/50 focus:border-transparent focus:outline-none focus:ring-[1px] focus:ring-green-500 focus:ring-opacity-60"
+              placeholder="Lugar de estudios"
             />
           </div>
-
+          <div class="flex flex-row gap-4">
+            <div className="relative w-full">
+              <span className="absolute inset-y-0 left-0 flex items-center pl-3">
+                <FontAwesomeIcon
+                  icon={faUserGraduate}
+                  className="h-5 w-5 text-white/50"
+                />
+              </span>
+              <select
+              id="courseInput"
+              type="text"
+              required
+              className="disabled:text-gray-200 block w-full rounded-lg border border-white/20 bg-white/5 p-2.5 pl-10 font-light text-gray-300 placeholder-white/50 focus:border-transparent focus:outline-none focus:ring-[1px] focus:ring-green-500 focus:ring-opacity-60"
+            >
+                <option disabled selected hidden>Nivel de estudios</option>
+                <option value="universidad">Universitarios</option>
+                <option value="fp">Formación Profesional</option>
+                <option value="secundaria">secundaria</option>
+                <option value="otros">otros</option>
+              </select>
+            </div>
+            <div className="relative w-full">
+              <span className="absolute inset-y-0 left-0 flex items-center pl-3">
+                <FontAwesomeIcon
+                  icon={faUserGraduate}
+                  className="h-5 w-5 text-white/50"
+                />
+              </span>
+              <select
+              id="courseInput"
+              type="text"
+              required
+              className="disabled:text-gray-200 block w-full rounded-lg border border-white/20 bg-white/5 p-2.5 pl-10 font-light text-gray-300 placeholder-white/50 focus:border-transparent focus:outline-none focus:ring-[1px] focus:ring-green-500 focus:ring-opacity-60"
+            >
+                <option disabled selected hidden>Curso</option>
+                <option value="1">1º</option>
+                <option value="2">2º</option>
+                <option value="3">3º</option>
+                <option value="4">4º</option>
+                <option value="no-aplica">No aplica</option>
+              </select>
+            </div>
+          </div>
           <div className="relative">
             <span className="absolute inset-y-0 left-0 flex items-center pl-3">
               <FontAwesomeIcon
-                icon={faEnvelope}
+                icon={faLocationPin}
                 className="h-5 w-5 text-white/50"
               />
             </span>
             <input
-              id="emailInput"
-              type="email"
+              id="courseInput"
+              type="text"
               required
               className="block w-full rounded-lg border border-white/20 bg-white/5 p-2.5 pl-10 font-light text-gray-300 placeholder-white/50 focus:border-transparent focus:outline-none focus:ring-[1px] focus:ring-green-500 focus:ring-opacity-60"
-              placeholder="Un correo electrónico"
+              placeholder="Ciudad de residencia"
             />
+          </div>
+          <div className="relative">
+            <span className="absolute inset-y-0 left-0 flex items-center pl-3">
+              <FontAwesomeIcon
+                icon={faUserGraduate}
+                className="h-5 w-5 text-white/50"
+              />
+            </span>
+            <input
+              id="courseInput"
+              type="text"
+              required
+              className="block w-full rounded-lg border border-white/20 bg-white/5 p-2.5 pl-10 font-light text-gray-300 placeholder-white/50 focus:border-transparent focus:outline-none focus:ring-[1px] focus:ring-green-500 focus:ring-opacity-60"
+              placeholder="Género"
+            />
+          </div>
+          <div className="relative">
+            <span className="absolute inset-y-0 left-0 flex items-center pl-3">
+              <FontAwesomeIcon
+                icon={faWheatAwnCircleExclamation}
+                className="h-5 w-5 text-white/50"
+              />
+            </span>
+            <select
+              id="courseInput"
+              type="text"
+              required
+              className="disabled:text-gray-200 block w-full rounded-lg border border-white/20 bg-white/5 p-2.5 pl-10 font-light text-gray-300 placeholder-white/50 focus:border-transparent focus:outline-none focus:ring-[1px] focus:ring-green-500 focus:ring-opacity-60"
+            >
+              <option disabled selected hidden>Restricciones alimentarias</option>
+              <option value="nada">Sin restricciones</option>
+              <option value="vegano">Vegano</option>
+              <option value="vegetariano">Vegetariano</option>
+              <option value="sin-gluten">Sin glúten</option>
+            </select>
+          </div>
+          <div className="relative">
+            <span className="absolute inset-y-0 left-0 flex items-center pl-3">
+              <FontAwesomeIcon
+                icon={faShirt}
+                className="h-5 w-5 text-white/50"
+              />
+            </span>
+            <select
+              id="courseInput"
+              type="text"
+              required
+              className="disabled:text-gray-200 block w-full rounded-lg border border-white/20 bg-white/5 p-2.5 pl-10 font-light text-gray-300 placeholder-white/50 focus:border-transparent focus:outline-none focus:ring-[1px] focus:ring-green-500 focus:ring-opacity-60"
+            >
+              <option disabled selected hidden>Talla de camiseta</option>
+              <option value="S">S</option>
+              <option value="M">M</option>
+              <option value="L">L</option>
+              <option value="XL">XL</option>
+            </select>
+          </div>
+          <div>
+            <input
+              id="creditosInput"
+              type="checkbox"
+              required
+              className="text-gray-300"
+            />
+            <label htmlFor="creditosInput" className="text-base"> ¿Solicitar créditos ECTS?</label>
+          </div>
+          <div>
+            <input
+              id="creditosInput"
+              type="checkbox"
+              required
+              className="text-gray-300"
+            />
+            <label htmlFor="creditosInput" className="text-base"> Acepto el código de conducta y los términos del evento</label>
+          </div>
+          <div>
+            <input
+              id="creditosInput"
+              type="checkbox"
+              required
+              className="text-gray-300"
+            />
+            <label htmlFor="creditosInput" className="text-base"> Acepto que se me podrá solicitar justificar la condición de estudiante antes del inicio del evento.</label>
           </div>
 
           <button
@@ -96,7 +293,7 @@ export default function Form() {
             className="hover group relative w-full rounded-lg bg-green-900 p-2.5 text-center font-light text-white/75"
             disabled={disabled}
           >
-            Unirse a la waitlist
+            Registrarme en HackUDC
             <div className="absolute inset-y-0 right-4 flex items-center transition-all group-hover:right-3">
               <FontAwesomeIcon
                 icon={faArrowRight}
